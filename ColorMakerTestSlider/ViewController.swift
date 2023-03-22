cfa3c3a9ff2a1ae45d15236ea3ca0947f9985120
Marcello Gonzatto Birkan <82703050+MelloBirkan@users.noreply.github.com>
2023-03-21 22:42:42 -0300

    Add files via upload

---
+++ ColorMakerTestSlider/ViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set colorView on launch
+        changeColorComponent()
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent(_ sender: UISlider? = nil) {
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redControl == nil {
+            return
+        }
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+                
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
